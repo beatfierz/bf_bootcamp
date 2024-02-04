8918db24c63ed82fbee91f6e29d723d1237a1a35
Beat Fierz <beat.fierz@epfl.ch>
2024-02-04 15:34:00 +0100

    Initial commit of bootcamp utilities.

---
+++ bf_bootcamp/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for utilities for bootcamp."""
+
+from .na_utils import *
+from .bioinfo_dicts import *
+
+__author__ = 'Beat Fierz'
+__email__ = 'beat.fierz@epfl.ch'
+__version__ = '0.0.1'
